0ed75862e904572a398f6fc103aa5052d48932c7
debugzee <142580639+debugzee@users.noreply.github.com>
2023-09-18 12:33:44 +0500

    Add API Stuff

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -5,19 +5,31 @@
 
 module.exports = {
   Query: {
-    
+    pets(_, { input }, ctx) {
+      return ctx.models.Pet.findMany(input);
+    },
+    pet(_, { input }, ctx) {
+      return ctx.models.Pet.findOne(input);
+    },
   },
   Mutation: {
-    
+    pet(_, { input }, ctx) {
+      return ctx.models.Pet.create(input);
+    },
   },
   Pet: {
     img(pet) {
-      return pet.type === 'DOG'
-        ? 'https://placedog.net/300/300'
-        : 'http://placekitten.com/300/300'
-    }
+      return pet.type === "DOG"
+        ? "https://placedog.net/300/300"
+        : "http://placekitten.com/300/300";
+    },
+    user(_, __, ctx) {
+      return ctx.models.User.findOne();
+    },
   },
   User: {
-    
-  }
-}
+    pets(user, __, ctx) {
+      return ctx.models.Pet.findMany({ user: user.id });
+    },
+  },
+};
--- api/src/schema.js
+++ api/src/schema.js
@@ -1,11 +1,44 @@
-const { gql } = require('apollo-server')
+const { gql } = require("apollo-server");
 
 /**
  * Type Definitions for our Schema using the SDL.
  */
 const typeDefs = gql`
-  
-
+  enum PetTypes {
+    Dog
+    Cat
+    Fish
+    Bunny
+  }
+  type User {
+    id: ID!
+    username: String!
+    pets: [Pet]!
+  }
+  type Pet {
+    id: ID!
+    createdAt: String!
+    name: String!
+    type: PetTypes!
+    img: String
+    user: User!
+  }
+  input PetsInput {
+    type: String
+    id: ID
+  }
+  type Query {
+    pets(input: PetsInput): [Pet]!
+    pet(input: PetsInput): Pet
+  }
+  input PetCreateInput {
+    name: String!
+    type: PetTypes!
+    user: ID!
+  }
+  type Mutation {
+    pet(input: PetCreateInput!): Pet!
+  }
 `;
 
-module.exports = typeDefs
+module.exports = typeDefs;
